177be95ff01da51e4ebc852e3e19633f975f57d5
rawendra <rawendra@gmail.com>
2024-02-03 18:10:24 +0530

    udpate

---
+++ index.js
@@ -0,0 +1,7 @@
+function displayString(string) {
+  return `you entered this : ${string}`;
+}
+
+const result =displayString("Rawendra Singh Kushwaha")
+console.log(result)
+module.exports = displayString;
